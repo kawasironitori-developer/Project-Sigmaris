ec5d1533d138847329bd6c0442a08298ed2b53d3
uthuyomi <kaiseif4e@gmail.com>
2025-11-07 09:37:20 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // 🧩 本番（Vercel）では console.* を完全削除
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
 
 export default nextConfig;
